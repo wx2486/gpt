4af65a00cd6b743866e40ca028c97028f6a08184
Xiang Wu <xiangw.ahu@gmail.com>
2023-05-27 17:57:13 +0800

    Update app.py

--- exam/app.py
+++ exam/app.py
@@ -6,4 +6,4 @@ def home():
     return "Hello, Docker!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/gpt.wx2486.me/fullchain.pem', '/etc/letsencrypt/live/gpt.wx2486.me/privkey.pem'))
+    app.run(host='0.0.0.0', port=443, ssl_context=('fullchain.pem', 'privkey.pem'))
